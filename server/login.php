576053cb8dd8bc02e3f78b9b14666b9766a31e5d
McKinney, Dalton William <dawmckin@iu.edu>
2020-02-25 17:36:51 -0500

    added login.php

---
+++ server/login.php
@@ -0,0 +1,23 @@
+<?php
+
+  include_once('db_connect.php');
+
+  $names = array();
+  $googleid = $_GET['gid'];
+
+  $sql = "SELECT gid FROM reg_users WHERE gid=$googleid;";
+
+  $stmt = $conn->prepare($sql);
+
+  $stmt->execute();
+
+  $stmt->bind_result($gid);
+
+  while ($stmt->fetch()) {
+    $temp = $gid;
+
+    array_push($names, $temp);
+  }
+
+  echo json_encode($names);
+?>
