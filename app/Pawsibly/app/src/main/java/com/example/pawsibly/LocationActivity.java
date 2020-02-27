c73f2cb1ea55f5d8226e6b251791db444a88b233
mjhan <mjhan@iu.edu>
2020-02-27 11:35:56 -0500

    location updated

---
+++ app/Pawsibly/app/src/main/java/com/example/pawsibly/LocationActivity.java
@@ -0,0 +1,18 @@
+private final LocationListener mLocationListener = new LocationListener() {
+@Override
+public void onLocationChanged(final Location location) {
+        //your code here
+        }
+        };
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+        LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
