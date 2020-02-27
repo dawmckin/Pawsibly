0520be0d25fc91e350b75005ee9847bacfe529e2
mjhan <mjhan@iu.edu>
2020-02-27 13:25:09 -0500

    updated location settings

--- app/Pawsibly/app/src/main/java/com/example/pawsibly/LocationActivity.java
+++ app/Pawsibly/app/src/main/java/com/example/pawsibly/LocationActivity.java
@@ -1,17 +1,78 @@
-private final LocationListener mLocationListener = new LocationListener() {
-@Override
-public void onLocationChanged(final Location location) {
-        //your code here
-        }
-        };
+package com.sample.foo.simplelocationapp;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
+    TextView longitudeValueBest, latitudeValueBest;
+    TextView longitudeValueGPS, latitudeValueGPS;
+    TextView longitudeValueNetwork, latitudeValueNetwork;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        longitudeValueBest = (TextView) findViewById(R.id.longitudeValueBest);
+        latitudeValueBest = (TextView) findViewById(R.id.latitudeValueBest);
+        longitudeValueGPS = (TextView) findViewById(R.id.longitudeValueGPS);
+        latitudeValueGPS = (TextView) findViewById(R.id.latitudeValueGPS);
+        longitudeValueNetwork = (TextView) findViewById(R.id.longitudeValueNetwork);
+        latitudeValueNetwork = (TextView) findViewById(R.id.latitudeValueNetwork);
+    }
 
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
-        LOCATION_REFRESH_DISTANCE, mLocationListener);
-        }
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
 
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("ENABLE LOCATION")
+                .setMessage("Your location settings is set to 'OFF'.\nPlease Enable Location to " +
+                        "get full functionality of Pawsibly")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
