cb59e561fcc6cfc47e6136aa571ef998558a4a06
sdgreath <sdgreath@indiana.edu>
2020-03-06 18:46:37 -0500

    created breeder home screen

---
+++ app/Pawsibly/app/src/main/java/com/example/pawsibly/BreederProfieActivity.java
@@ -0,0 +1,14 @@
+package com.example.pawsibly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BreederProfieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breeder_profie);
+    }
+}
