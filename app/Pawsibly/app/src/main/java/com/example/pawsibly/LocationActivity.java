e6b94ca6e5b20f223628c3bf9abfd3c3341a1794
mjhan <mjhan@iu.edu>
2020-03-08 16:03:56 -0400

    Updated Location

--- app/Pawsibly/app/src/main/java/com/example/pawsibly/LocationActivity.java
+++ app/Pawsibly/app/src/main/java/com/example/pawsibly/LocationActivity.java
@@ -1,7 +1,48 @@
 package com.example.pawsibly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class LocationActivity extends AppCompatActivity {
+
+    private static final String TAG = LocationActivity.class.getSimpleName();
+
+    private static final int UPDATE_INTERVAL = 5000; // 5 seconds
+
+    FusedLocationProviderClient locationProviderClient;
+    LocationRequest locationRequest;
+    private Location currentLocation;
+
+    private int LOCATION_PERMISSION = 100;
+
+    Button enable_location_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        buttonEnableLocation = (Button)findViewById(R.id.enable_location_btn);
+    }
 
 }
