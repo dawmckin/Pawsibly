d2a12f0eda2ad76abe186a5bc6160d02a79a0a17
sdgreath <sdgreath@indiana.edu>
2020-04-17 22:05:23 -0400

    created shelter/breeder message user button on selectedAnimalUser

--- app/Pawsibly/app/src/main/java/com/example/pawsibly/SelectedAnimalUser.java
+++ app/Pawsibly/app/src/main/java/com/example/pawsibly/SelectedAnimalUser.java
@@ -2,7 +2,9 @@ package com.example.pawsibly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
